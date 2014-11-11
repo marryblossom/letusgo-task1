fdeb335df8f59b2edd52451fdbf7d4b498fa2395
Zhaoyu Wei <weixiaobo88@gmail.com>
2014-11-11 15:24:23 +0800

    zy: refactor js code
    extract getdate out as utils

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,31 @@
 $(document).ready(function () {
+    var feature = (function () {
+        var initItems = function () {
+            var items = loadAllItems();
 
-    (function () {
-        var items = loadAllItems();
-
-        _(items).each(function (item) {
-            var addCart = '<button class="btn btn-primary btm-sm">加入购物车</button>';
-            var listItem = $('<tr class="list-item" data-barcode="' + item.barcode + '">\
-                            <td class="item-name">' + item.name + '</td>\
+            _(items).each(function (item) {
+                var addCart = '<button>加入购物车</button>';
+                var listItem = $('<tr>\
+                            <td>' + item.name + '</td>\
                             <td>' + item.price + '</td>\
                             <td>' + item.unit + '</td>\
                             <td>' + addCart + '</td>\
                           </tr>');
-            $('#item-table').append(listItem);
-        });
-
-        var dateDigitToString = function (num) {
-            return num < 10 ? '0' + num : num;
-        };
-        var getDate = function () {
-            var currentDate = new Date();
-            var year = dateDigitToString(currentDate.getFullYear());
-            var month = dateDigitToString(currentDate.getMonth() + 1);
-            var date = dateDigitToString(currentDate.getDate());
-            var hour = dateDigitToString(currentDate.getHours());
-            var minute = dateDigitToString(currentDate.getMinutes());
-            var second = dateDigitToString(currentDate.getSeconds());
-            return  year + '年' + month + '月' + date + '日 ' + hour + ':' + minute + ':' + second;
+                $('#item-table').append(listItem);
+            });
         };
 
-        $("#pay-date").text(getDate());
-    })($);
+        var printDate = function() {
+            $("#pay-date").text(utils.getDate());
+        };
 
+        return {
+            init: initItems,
+            printDate: printDate
+        };
+    })();
+
+    feature.init();
+    feature.printDate();
 });
 
