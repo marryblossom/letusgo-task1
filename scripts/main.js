86868d2f05c87d6df013da8025a11a925ab09d37
tianmali <408267415@qq.com>
2014-11-20 10:02:20 +0800

    realize cart number plus when click button

--- scripts/main.js
+++ scripts/main.js
@@ -7,14 +7,19 @@ $(document).ready(function () {
                 var addCart = '<button>加入购物车</button>';
                 var listItem = $('<tr>\
                             <td>' + item.name + '</td>\
-                            <td>' + item.price + '</td>\
+                            <td class="price">' + item.price + '</td>\
                             <td>' + item.unit + '</td>\
                             <td>' + addCart + '</td>\
                           </tr>');
                 $('#item-table').append(listItem);
             });
         };
-
+	var cartPath = $("#count");
+	$('#item-table').on('click', 'button', function(){
+	//var path = $(this).closest('#item-table').parent().siblings().find('#count');
+		var count = +cartPath.text()+1;
+		cartPath.text(count);
+	});
         var printDate = function() {
             $("#pay-date").text(utils.getDate());
         };
